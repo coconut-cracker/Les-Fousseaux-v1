8ef33ac801def4a5c974922ce0f4ed8774b8a87b
William Forster <60694070+coconut-cracker@users.noreply.github.com>
2020-08-04 09:24:11 +0200

    Form validation added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const { help } = require("yargs");
+// const { help } = require("yargs");
 
 function toggleGallery() {
   let showHideBtn = Array.from(
@@ -9,3 +9,26 @@ function toggleGallery() {
     e.classList.toggle("hidden");
   });
 }
+
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
